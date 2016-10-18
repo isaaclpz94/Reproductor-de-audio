07a9c316e117e2e182e78a50e6b0ce80a76241f8
isaaclpz94 <isaaclopez2894@hotmail.com>
2016-10-18 19:22:42 +0200

    Reproductor de audio

--- ReproductorAudio/ViewController.swift
+++ ReproductorAudio/ViewController.swift
@@ -7,19 +7,55 @@
 //
 
 import UIKit
+import AVFoundation
+
+var reproductorAudio = AVAudioPlayer()
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let ubicacionAudio = NSBundle.mainBundle().pathForResource("Foyone", ofType: "mp3")
+        var elAudio = NSURL(fileURLWithPath: ubicacionAudio!)
+        reproductorAudio = AVAudioPlayer(contentsOfURL: elAudio, error: nil)
+        reproductorAudio.prepareToPlay()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func btnPlay(sender: UIButton) {
+        reproductorAudio.play()
+        controlBotones(true)
+    }
+    
+    @IBAction func btnPause(sender: UIButton) {
+        reproductorAudio.pause()
+        controlBotones(false)
+    }
+    
+    @IBAction func btnStop(sender: UIButton) {
+        reproductorAudio.stop()
+        reproductorAudio.currentTime = 0
+        controlBotones(false)
+    }
 
-
+    
+    @IBAction func SliderVolumen(sender: AnyObject) {
+        reproductorAudio.volume = outVolumen.value
+    }
+    
+    @IBOutlet var outVolumen: UISlider!
+    @IBOutlet var outPlay: UIButton!
+    @IBOutlet var outPause: UIButton!
+    @IBOutlet var outStop: UIButton!
+    
+    func controlBotones(reproduciendo:Bool){
+        outPlay.enabled = !reproduciendo
+        outPause.enabled = reproduciendo
+        outStop.enabled = reproduciendo
+    }
 }
 
